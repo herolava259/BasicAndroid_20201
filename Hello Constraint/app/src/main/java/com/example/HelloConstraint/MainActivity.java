819d909b23d46e7751f56b0d492ef90b03485ab7
Herolava <chopperman259@gmail.com>
2021-10-10 16:01:02 +0700

    homework updat w2

--- Hello Constraint/app/src/main/java/com/example/HelloConstraint/MainActivity.java
+++ Hello Constraint/app/src/main/java/com/example/HelloConstraint/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.HelloConstraint;
 
+import static HelloConstraint.R.color.button_material_dark;
+import static HelloConstraint.R.color.design_default_color_primary_dark;
+import static HelloConstraint.R.color.purple_500;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,15 +36,40 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
+
+    @SuppressLint("ResourceAsColor")
     public void countUp(View view){
-        mCount++;
+        Set_ButtonCount(false);
         if(mShowCount != null) mShowCount.setText(Integer.toString(mCount));
     }
 
+    @SuppressLint("ResourceAsColor")
     public void resetZero(View view) {
-        mCount =0;
+
+        Set_ButtonCount(true);
+        //Button btn = (Button)view;
+        //btn.setBackgroundColor(purple_200);
         if(mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
         }
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private void Set_ButtonCount(boolean reset ){
+        View view = findViewById(R.id.button_count);
+        if(!reset){
+            mCount++;
+
+        }else {
+            mCount = 0;
+
+        }
+        if(mCount%2 == 0){
+            view.setBackgroundColor(getResources().getColor(R.color.teal_200));
+
+        }else {
+            view.setBackgroundColor(R.color.purple_200);
+        }
+
     }
 }
